c8b218cd7c68457500bc189c7c7bccfd0926dc7a
tarukosu <u.au.sky.future12@gmail.com>
2015-01-31 14:25:29 +0900

    update heartbeat

--- jsk_network_tools/scripts/heartbeat_master.py
+++ jsk_network_tools/scripts/heartbeat_master.py
@@ -12,6 +12,10 @@ class Master():
         self.pub_round_time = rospy.Publisher("heartbeat/round_time", Float32)
         rospy.Subscriber("heartbeat/response", HeartbeatResponse, self.callback)
         self.rate = 1.0
+        self.timer_update = False
+        self.delay = rospy.Time(0)
+
+        rospy.Timer(rospy.Duration(1), self.timer_callback)
 
         while not rospy.is_shutdown():
             msg = Heartbeat()
@@ -20,11 +24,22 @@ class Master():
             self.pub_request.publish(msg)
             rospy.sleep(1.0/self.rate)
 
+    def timer_callback(self, event):
+        if self.timer_update:
+            self.delay = self.delay + rospy.Duration(1)
+            upload_time_msg = Float32(self.delay.to_sec())
+            self.pub_upload_time.publish(upload_time_msg)
+        self.timer_update = True
+
+
     def callback(self, data):
+        self.timer_update = False
         now = rospy.Time.now()
         upload_time = data.header.stamp - data.heartbeat.header.stamp
         download_time = now - data.header.stamp
         round_time = now - data.heartbeat.header.stamp
+        
+        self.delay = upload_time
 
         rospy.loginfo("upload_time: %f" % (upload_time.to_sec()))
         rospy.loginfo("download_time: %f" % (download_time.to_sec()))
